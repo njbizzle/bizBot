4a7fe1f920d8840174ebb55c233bba206d7359e8
nmurphy <nmurphy.mine@gmail.com>
2024-04-16 22:15:29 -0700

    -bash

--- src/testBot.py
+++ src/testBot.py
@@ -1,4 +1,9 @@
-import discord, os
+import discord, os, load_dotenv
+
+APP_ROOT = os.path.join(os.path.dirname(__file__))
+ENV_PATH = str(APP_ROOT.split("src", 1)[0]) + ".env"
+
+load_dotenv.load_dotenv(ENV_PATH)
 
 class MyClient(discord.Client):
     async def on_ready(self):
