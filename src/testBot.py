7f784c2a35c91c8bad04690230f5221809cef70a
nmurphy <nmurphy.mine@gmail.com>
2024-04-16 23:31:24 -0700

    -bash

--- src/testBot.py
+++ src/testBot.py
@@ -4,25 +4,25 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(
-    intents=intents,
-    status=discord.Status.dnd,
-    activity=discord.CustomActivity(
-        name="test",
-        emoji=discord.PartialEmoji(name="skull")
-    )
+
 )
 
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
+class BizBot(discord.Client):
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+    async def on_ready(self):
+        print(f'We have logged in as {client.user}')
 
-    if message.content.startswith("yo"):
-        await message.channel.send("Hello!")
+    async def on_message(self, message):
+        if message.author == client.user:
+            return
+
+        if message.content.startswith("yo"):
+            await message.channel.send("Hello!")
+
+    async def on_message_delete(self, message):
+        await message.channel.send(
+            f"\"{message.content}\" - {message.author.name}"
+        )
 
 
 def start_bot(token: str):
