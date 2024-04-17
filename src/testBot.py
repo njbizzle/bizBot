47d16b5ce03c8ee3eeb47652e0ded998fe7ce3ec
nmurphy <nmurphy.mine@gmail.com>
2024-04-16 23:20:28 -0700

    -bash

--- src/testBot.py
+++ src/testBot.py
@@ -8,7 +8,7 @@ client = discord.Client(
     status=discord.Status.dnd,
     activity=discord.CustomActivity(
         name="test",
-        emoji=discord.PartialEmoji.from_str("<:duckysleep:1059974724588945479>")
+        emoji=discord.PartialEmoji(name="skull")
     )
 )
 
